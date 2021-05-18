bacf46d565e5ca628568dc4648a5fbc6921007d2
HiBang15 <tuanlongaa@gmail.com>
2021-05-18 19:47:43 +0700

    update code

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/HiBang15/golang-api-base-tulpo.git/api/rest/router"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	// load config from .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+// @securityDefinitions.apikey bearerAuth
+// @in header
+// @name Authorization
+func main() {
+	//run Rest API
+	fmt.Println("Start run REST API .....")
+	router.Start(os.Getenv("ENVIRONMENT"))
+}
